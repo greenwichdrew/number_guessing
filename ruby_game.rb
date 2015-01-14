dbd05cc82d9c505cb1232dd3ab4b76e56afe3c5a
Drew Smith <greenwichdrew@gmail.com>
2015-01-13 22:20:52 -0500

    first commit

---
+++ ruby_game.rb
@@ -0,0 +1,32 @@
+def greeting
+  puts "Welcome to secret number game. What is your name?"
+  name = gets.chomp
+  puts "Hello #{name} let's play"
+  puts "Pick a number between 1 and 100. You have 5 chances"
+end
+
+
+def guess_number(secret_number)
+  user_number = gets.chomp.to_i
+  chances = 5
+
+  if user_number == secret_number
+    puts "That's the secret number. You win!"
+    exit
+  elsif user_number > secret_number
+    puts "You are to high. Guess again"
+    puts "You have #{chances -1} guesses left."
+  elsif user_number < secret_number
+    puts "You are to low. Guess again"
+    puts "You have #{chances -1} guesses left."
+  end
+end
+
+
+greeting
+chances = 5
+random_number = rand(1..100)
+puts "#{random_number} is secret number"
+while chances > 0
+  guess_number(random_number)
+end
